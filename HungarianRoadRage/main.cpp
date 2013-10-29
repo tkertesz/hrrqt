cfa13c59378a7d4d99df53c1e926f426c0df5019
Tibor Kertesz <hummerdikk@gmail.com>
2013-10-29 15:02:34 +0100

    Added plus things to imshow

--- HungarianRoadRage/main.cpp
+++ HungarianRoadRage/main.cpp
@@ -33,17 +33,26 @@ using namespace cv;
 using namespace std;
 
 int main() {
+
+Mat cameraFrame;
 VideoCapture stream1(0);   //0 is the id of video device.0 if you have only one camera.
 
 if (!stream1.isOpened()) { //check if video device has been initialised
 cout << "cannot open camera";
 }
-
+cvNamedWindow("hello");
 //unconditional loop
 while (true) {
-Mat cameraFrame;
+
 stream1.read(cameraFrame);
-imshow("cam", cameraFrame);
+try
+{
+    imshow("hello", cameraFrame);
+}
+catch (Exception& e)
+{
+}
+
 if (waitKey(30) >= 0)
 break;
 }
